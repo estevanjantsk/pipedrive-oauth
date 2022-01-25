088b640b7802922ef21b99110f6075226ab74119
Estevan Jantsk <estevan.jantsk@gmail.com>
2022-01-25 11:17:41 -0300

    add JWT Callback

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,4 +8,13 @@ export default NextAuth({
 			clientSecret: process.env.PIPEDRIVE_CLIENT_SECRET || '',
 		})
 	],
+	callbacks: {
+		async jwt({ token, account }) {
+			// Persist the OAuth access_token to the token right after signin
+			if (account) {
+				token.accessToken = account.access_token
+			}
+			return token
+		}
+	}
 })
