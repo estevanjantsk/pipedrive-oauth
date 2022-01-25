5305a34eb7f203c0db2d9c12942e8807e7c5095f
Estevan Jantsk <estevan.jantsk@gmail.com>
2022-01-25 11:16:06 -0300

    add sample api request

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from "next-auth/react"
 
 type Data = {
-  name: string
+  content?: string,
+  error?: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const session = await getSession({ req })
+
+  if (session) {
+    res.send({ content: "This is protected content. You can access this content because you are signed in.",
+    })
+  } else {
+    res.send({
+      error: "You must be sign in to view the protected content on this page.",
+    })
+  }
 }
